acdf7562162e48bead3a3189c0097211c6bc80ed
Camille <145802391+aceillm@users.noreply.github.com>
2023-10-16 13:04:53 +0200

    Add files via upload

---
+++ formulaire.js
@@ -0,0 +1,46 @@
+/*
+ *! Form Generator v2.0 – Generated form's value replacer
+ *
+ * Developed by Luiz Felipe Gonçalves <https://luizfelipe.dev>
+ *
+ * All Forumotion Support Forums may USE this code.
+ * DO NOT REDISTRIBUTE OR EDIT WITHOUT THE AUTHOR'S CONSENT.
+ *
+ * Do not modify the origin of any script or style sheet without contacting
+ * the generator's author (Luiz Felipe Gonçalves - https://luizfelipe.dev/).
+ *
+ * Copyright (c) Luiz Felipe Gonçalves. 2017-current.
+ */
+
+/*globals jQuery*/
+
+(function ($) {
+    'use strict';
+  
+    $(function () {
+      var $textarea = $('#fa-generated-message');
+      var $title = $('#fa-generated-title');
+      var $form = $('#fa-generated-form');
+  
+      $form.on('submit', function (event) {
+  
+        if ($textarea.length === 0) {
+          event.preventDefault();
+          alert('[Erro Codigo 001 | Form Functions] Tente novamente!');
+          return false;
+        }
+  
+        $textarea.val($textarea.val().replace(/\{\{fields(\w+)\}\}/gi, function (text, match) {
+          return $('#fields' + match).val();
+        }));
+  
+        if ($title.length === 0) {
+          return;
+        }
+  
+        $title.val($title.val().replace(/\{\{fields(\w+)\}\}/gi, function (text, match) {
+          return $('#fields' + match).val();
+        }));
+      });
+    });
+  }(jQuery));
